85c2b7b1f0d3ec4956ac24dfa28dc4a411215945
natalia-panagi <natalia.panagi@aol.com>
2026-02-23 17:04:00 +0200

    Added search funtionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#current-t");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+    let apiKey = "fb6a7o33d928a9e256t114bb04022298"; 
+    let apiUrl =
+      `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity (searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+
